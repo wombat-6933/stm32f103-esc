cbb41edb69c982150c9afa88e63634be40876873
jose <josegu05@ucm.es>
2025-05-13 10:35:58 +0200

    sin warnings ni errores

--- src/vector_table.h
+++ src/vector_table.h
@@ -5,16 +5,16 @@
 void default_Handler(void);
 void reset_handler(void);
 void nmi_handler(void) __attribute__ ((weak, alias("default_Handler")));
-uint32_t * vector_table[] __attribute__ ((section("._vector_table"))) =
+uint32_t vector_table[] __attribute__ ((section("._vector_table"))) =
 {
-	0x0,
-	reset_handler,
-	nmi_handler,
-	default_Handler,
-	default_Handler,
-	default_Handler,
-	default_Handler,
-	default_Handler,
+(uint32_t)	0x0,
+(uint32_t)	reset_handler,
+(uint32_t)	nmi_handler,
+(uint32_t)	default_Handler,
+(uint32_t)	default_Handler,
+(uint32_t)	default_Handler,
+(uint32_t)	default_Handler,
+(uint32_t)	default_Handler,
 }; 
 
 void default_Handler(void)
