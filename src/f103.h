0edbb17193dc34d2545c59f092e963ff612050c5
Jose Luis <jose@11LAP5CD3372QH8.indra.es>
2025-06-05 14:11:36 +0200

    incluidos RCC registers, empezar GPIOS, probar en placa

--- src/f103.h
+++ src/f103.h
@@ -6,4 +6,13 @@
 #define RCC_REG_BASE 0x40021000
 #define RCC_CR (* (uint32_t *) (RCC_REG_BASE + 0x00))
 #define RCC_CFGR (* (uint32_t *) (RCC_REG_BASE + 0x04))
+#define RCC_CIR (* (uint32_t *) (RCC_REG_BASE + 0x08))
+#define RCC_APB2RSTR (* (uint32_t *) (RCC_REG_BASE + 0x0C))
+#define RCC_APB1RSTR (* (uint32_t *) (RCC_REG_BASE + 0x10))
+#define RCC_AHBENR (* (uint32_t *) (RCC_REG_BASE + 0x14))
+#define RCC_APB2ENR (* (uint32_t *) (RCC_REG_BASE + 0x18))
+#define RCC_APB1ENR (* (uint32_t *) (RCC_REG_BASE + 0x1C))
+#define RCC_BDCR (* (uint32_t *) (RCC_REG_BASE + 0x20))
+#define RCC_CFGR2 (* (uint32_t *) (RCC_REG_BASE + 0x2C))
+
 #endif
--- src/main.c
+++ src/main.c
@@ -3,9 +3,11 @@
 int main(void)
 {
 	
-	uint32_t rcc_rc;
+	uint32_t rcc_rc, rcc_cfgr, rcc;
 
 	rcc_rc = RCC_CR;
+	rcc_cfgr = RCC_CFGR;
+	rcc = RCC_AHBENR;
 	while (1);
    return 0;
 }
