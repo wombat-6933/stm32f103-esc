6a626ac8ca38d3f98e7b310e00917e006f442898
jose-pc <josegu05@ucm.es>
2025-06-18 22:26:17 +0200

    merge con el doc-debug, probado en placa, revisar codigo partes marcadas

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "f103.h"
+#define WAIT_TIME 0x00010000
 
 int main(void)
 {
@@ -14,9 +15,9 @@ int main(void)
 	while (1)
 	{
 		GPIOA_ODR |= (1 << GPIO_PIN_2);
-		for (uint32_t i=0; i<0x01000000; i++);
+		for (uint32_t i=0; i< WAIT_TIME; i++);
 		GPIOA_ODR &= ~(1 << GPIO_PIN_2);
-		for (uint32_t i=0; i<0x01000000; i++);
+		for (uint32_t i=0; i< WAIT_TIME; i++);
 	}
 	while (1);
    return 0;
